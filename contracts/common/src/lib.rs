f0af922251f5dcf7120a1dd554a97e1fdd20724e
Gbolahan Akande <geakande@gmail.com>
2026-02-23 16:15:39 +0100

    feat: register meta_tx module in common

--- contracts/common/src/lib.rs
+++ contracts/common/src/lib.rs
@@ -1,7 +1,17 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+#[cfg(feature = "std")]
 pub mod consent;
+#[cfg(feature = "std")]
 pub mod keys;
+pub mod meta_tx;
+#[cfg(feature = "std")]
 pub mod multisig;
 
+#[cfg(feature = "std")]
 pub use consent::*;
+#[cfg(feature = "std")]
 pub use keys::*;
+pub use meta_tx::*;
+#[cfg(feature = "std")]
 pub use multisig::*;
